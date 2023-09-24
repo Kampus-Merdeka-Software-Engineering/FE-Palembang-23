ca202d8a842a2d97c13af0d48ad1cd56c7d837d9
Arya Radinsyah Fauzi <radinsyah.fauzi@gmail.com>
2023-09-24 10:00:58 -0700

    dropdown menu profile in navbar

---
+++ js/navbarProfile.js
@@ -0,0 +1,10 @@
+
+// membuat dropdown menu profile
+
+
+let subMenu = document.getElementById("subMenu");
+
+
+        function toggleMenu() {
+            subMenu.classList.toggle("open-menu");
+        }
