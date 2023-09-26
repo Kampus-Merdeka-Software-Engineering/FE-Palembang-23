aef297d604d022f59ee9b7f14559b9b6e86a8e4e
Arya Radinsyah Fauzi <radinsyah.fauzi@gmail.com>
2023-09-26 12:58:17 -0700

    membuat change profile section

--- js/navbarProfile.js
+++ js/navbarProfile.js
@@ -5,6 +5,44 @@
 let subMenu = document.getElementById("subMenu");
 
 
-        function toggleMenu() {
-            subMenu.classList.toggle("open-menu");
-        }
+function toggleMenu() {
+    subMenu.classList.toggle("open-menu");
+}
+
+
+
+
+// change page
+function showPage(pageNumber) {
+    // menyembunikan semua page
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => page.classList.remove('active'));
+
+    // menampilkan page yang dipilih
+    const selectedPage = document.getElementById(`page${pageNumber}`);
+    selectedPage.classList.add('active');
+}
+
+// pindah ke page 1
+showPage(1);
+
+
+
+// edit profile
+    function changeUsernameAndEmail() {
+        // dapatkan value baru dari dorm input
+        const newUsername = document.getElementById('username').value;
+        const newEmail = document.getElementById('email').value;
+
+        // Update profile-name and profile-email dengan value baru
+        document.getElementById('profile-name').textContent = newUsername;
+        document.getElementById('profile-email').textContent = newEmail;
+    }
+
+    // add event listener
+    const editProfileForm = document.getElementById('edit-profile-form');
+    editProfileForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        changeUsernameAndEmail(); 
+        
+    });
